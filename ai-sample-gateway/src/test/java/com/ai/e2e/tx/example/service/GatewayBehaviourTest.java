9eda04676fd56bf21c906deb4b5a45e712fbee0a
Pavel Jakovlev <pavel.jakovlev@nortal.com>
2021-03-25 14:17:10 +0200

    Remove stale code

--- ai-sample-gateway/src/test/java/com/ai/e2e/tx/example/service/GatewayBehaviourTest.java
+++
@@ -1,9 +0,0 @@
-package com.ai.e2e.tx.example.service;
-
-import com.nortal.healthcare.test.BehaviourRunner;
-import io.cucumber.junit.CucumberOptions;
-
-@CucumberOptions(extraGlue = { "com.nortal.healthcare.service.glue" })
-public class GatewayBehaviourTest extends BehaviourRunner {
-
-}
--- ai-sample-gateway/src/test/java/com/ai/e2e/tx/example/service/ServiceTestConfig.java
+++
@@ -1,7 +0,0 @@
-package com.ai.e2e.tx.example.service;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan("com.nortal.healthcare.service.components")
-public class ServiceTestConfig {
-}
